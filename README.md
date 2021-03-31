dc8b3ab406bfbfedece2f456da907bf2344210d6
sash-a <reallysasha@gmail.com>
2021-03-31 18:25:24 +0200

    Switch AntGatherEnv to use the mujoco like ant from pybullet-gym

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This package was created because all of the HRL locomotion envs are only available in mujoco. This is an implementation of as many as possible in pybullet. 
 
 ### Install
-`pip install hrl_pybullet_envs`
+`pip install hrl_pybullet_envs`  
+This project requires [pybullet-gym](https://github.com/benelot/pybullet-gym/) which must be installed separately.
 
 
 ### Envs:
--- hrl_pybullet_envs/envs/ant_gather/env.py
+++ hrl_pybullet_envs/envs/ant_gather/env.py
@@ -3,14 +3,12 @@ import warnings
 from typing import Dict
 
 import numpy as np
-# noinspection PyUnresolvedReferences
-import pybulletgym
-from pybullet_envs.gym_locomotion_envs import AntBulletEnv
+from pybulletgym.envs.mujoco.envs.locomotion.ant_env import AntMuJoCoEnv
 
 from hrl_pybullet_envs.envs.ant_gather.scene import GatherScene
 
 
-class AntGatherBulletEnv(AntBulletEnv):
+class AntGatherBulletEnv(AntMuJoCoEnv):
     FOOD = 'food'
     POISON = 'poison'
 
@@ -19,14 +17,15 @@ class AntGatherBulletEnv(AntBulletEnv):
                  n_poison=8,
                  world_size=(15, 15),
                  n_bins=10,
-                 sensor_range=8.,
+                 sensor_range=20.,
                  sensor_span=np.pi,
                  robot_coll_dist=1,  # this might be too easy
                  robot_object_spacing=2.,
                  dying_cost=-10,
                  respawn=True,
-                 render=False):
-        super().__init__(render=render)
+                 render=False,
+                 debug=False):
+        super().__init__()
         self._alive = True
         self.walk_target_x = 0
         self.walk_target_y = 0
@@ -37,6 +36,9 @@ class AntGatherBulletEnv(AntBulletEnv):
         self.sensor_range = sensor_range
         self.dying_cost = dying_cost
         self.robot_coll_dist = robot_coll_dist
+        self.state_len = 27
+
+        self.debug = debug
 
         # scene related
         self.stadium_scene: GatherScene = None
@@ -46,9 +48,9 @@ class AntGatherBulletEnv(AntBulletEnv):
         self.spacing = robot_object_spacing
         self.respawn = respawn
 
-        # removing angle to target and adding in yaw and food readings
+        # ignoring contact points
         self.observation_space = self.robot.observation_space
-        self.observation_space.shape = (self.observation_space.shape[0] - 2 + 1 + 2 * n_bins,)
+        self.observation_space.shape = (self.state_len + 2 * n_bins,)
 
     def create_single_player_scene(self, bullet_client):
         self.stadium_scene = GatherScene(bullet_client, 9.8, 0.0165 / 4, 4,
@@ -66,7 +68,7 @@ class AntGatherBulletEnv(AntBulletEnv):
 
         dists = {obj_id: self.sq_dist_robot(pos) for obj_id, pos in self.stadium_scene.all_items.items()}
         fr, pr = self.get_readings(dists)
-        return np.concatenate([r[:1], r[3:8], [self.robot.body_rpy[2]], r[8:], fr, pr])
+        return np.concatenate([r[:self.state_len], fr, pr])
 
     def step(self, a):
         self.robot.apply_action(a)
@@ -86,29 +88,30 @@ class AntGatherBulletEnv(AntBulletEnv):
 
         # removing angle to target and adding in yaw and food readings
         fr, pr = self.get_readings(dists)
-        state = np.concatenate([state[:1], state[3:8], [self.robot.body_rpy[2]], state[8:], fr, pr])
+        state = np.concatenate([state[:self.state_len], fr, pr])
+
 
         # state[0] is body height above ground, body_rpy[1] is pitch
         self._alive = float(self.robot.alive_bonus(state[0] + self.robot.initial_z, self.robot.body_rpy[1]))
-        done = self._isDone()
+        done = self._alive < 0
         if not np.isfinite(state).all():
             warnings.warn(f'~INF~ {state}')
             done = True
 
-        for i, f in enumerate(self.robot.feet):
-            contact_ids = set((x[2], x[4]) for x in f.contact_list())
-            if self.ground_ids & contact_ids:
-                # see Issue 63: https://github.com/openai/roboschool/issues/63
-                self.robot.feet_contact[i] = 1.0
-            else:
-                self.robot.feet_contact[i] = 0.0
+        # for i, f in enumerate(self.robot.feet):
+        #     contact_ids = set((x[2], x[4]) for x in f.contact_list())
+        #     if self.ground_ids & contact_ids:
+        #         # see Issue 63: https://github.com/openai/roboschool/issues/63
+        #         self.robot.feet_contact[i] = 1.0
+        #     else:
+        #         self.robot.feet_contact[i] = 0.0
 
         if self.robot_coll_dist <= 0:  # otherwise use distance based collision (this is harder)
             collided_ids = [cp[2] for cp in self._p.getContactPoints(self.robot.objects[0])]
             for coll_id in collided_ids:
                 food_reward += self.stadium_scene.reward_collision(coll_id, self.robot.body_real_xyz)
 
-        dead_rew = self.dying_cost if self._isDone() else 0
+        dead_rew = self.dying_cost if self._alive < 0 else 0
         return state, food_reward + dead_rew, bool(done), {'food_rew': food_reward, 'dead_rew': dead_rew}
 
     # modified from: rllab.envs.mujoco.gather.gather_env.py
@@ -149,8 +152,9 @@ class AntGatherBulletEnv(AntBulletEnv):
             intensity = 1.0 - dist / self.sensor_range
 
             # useful debug
-            # self._p.addUserDebugLine(self.robot_body.pose().xyz(), [ox, oy, 0], lifeTime=0.1,
-            #                          lineColorRGB=[1, 0, 0] if typ == self.POISON else [0, 1, 0])
+            if self.debug:
+                self._p.addUserDebugLine(self.robot_body.pose().xyz(), [ox, oy, 0], lifeTime=0.1,
+                                         lineColorRGB=[1, 0, 0] if typ == self.POISON else [0, 1, 0])
             if typ == self.FOOD:
                 food_readings[bin_number] = intensity
             elif typ == self.POISON:
@@ -161,4 +165,5 @@ class AntGatherBulletEnv(AntBulletEnv):
         return food_readings, poison_readings
 
     def sq_dist_robot(self, pos):
-        return (pos[0] - self.robot.body_real_xyz[0]) ** 2 + (pos[1] - self.robot.body_real_xyz[1]) ** 2
+        xyz = self.parts['torso'].get_pose()
+        return (pos[0] - xyz[0]) ** 2 + (pos[1] - xyz[1]) ** 2

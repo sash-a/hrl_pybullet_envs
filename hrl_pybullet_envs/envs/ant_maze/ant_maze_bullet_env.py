54e895f788c12d90ab5e7ddc1afa1a336ea7028c
sash-a <reallysasha@gmail.com>
2021-07-23 14:13:03 +0200

    Made targets settable in constructor

--- hrl_pybullet_envs/envs/ant_maze/ant_maze_bullet_env.py
+++ hrl_pybullet_envs/envs/ant_maze/ant_maze_bullet_env.py
@@ -1,13 +1,13 @@
-from typing import List
-
 import numpy as np
 from pybullet_envs.gym_locomotion_envs import AntBulletEnv
 
 from hrl_pybullet_envs.envs.ant_maze.maze_scene import MazeScene
-from hrl_pybullet_envs.envs.ant_maze.maze_utils import pol2cart, Point, intersection, quadrant
 from hrl_pybullet_envs.utils import debug_draw_point, PositionEncoding
 from gym.spaces import Box
 import gym
+
+_eval_target = [5, -3]
+_targets = ([-5, 3], [0, 3], [5, 3], [5, 0], _eval_target)
 
 
 class AntMazeBulletEnv(AntBulletEnv):
@@ -15,10 +15,8 @@ class AntMazeBulletEnv(AntBulletEnv):
     At evaluation time, we evaluate the agent only on its ability to reach (0,16). We define a “success” as being within
     an L2 distance of 5 from the target on the ultimate step of the episode. - Data efficient HRL
     """
-    eval_target = [5, -3]
-    targets = [[-5, 3], [-2.5, 3], [0, 3], [2.5, 3], [5, 3], [5, 0], eval_target]
 
-    def __init__(self, n_bins: int = 10, sensor_range: float = 5, sensor_span: float = 2 * np.pi,
+    def __init__(self, n_bins: int = 10, sensor_range: float = 5, sensor_span: float = 2 * np.pi, targets=_targets,
                  target_encoding: PositionEncoding = 0, tol=1.5, inner_rew_weight=0, seed=None, debug=0):
         super().__init__()
         self.robot.start_pos_x, self.robot.start_pos_y, self.robot.start_pos_z = -5, -3, 0.25
@@ -27,13 +25,14 @@ class AntMazeBulletEnv(AntBulletEnv):
         self.sensor_range = float(sensor_range)
         self.sensor_span = sensor_span
 
+        self.targets = targets
         self.tol = tol
         self.inner_rew_weight = inner_rew_weight
 
         if isinstance(target_encoding, int):
             target_encoding = PositionEncoding(target_encoding)
         self.target_encoding = target_encoding
-        self.target: np.ndarray = np.array(AntMazeBulletEnv.eval_target)
+        self.target: np.ndarray = np.array(_eval_target)
 
         self.rs, _ = gym.utils.seeding.np_random(seed)
 
@@ -86,7 +85,7 @@ class AntMazeBulletEnv(AntBulletEnv):
             super().reset()
 
         start_xyz = [self.robot.start_pos_x, self.robot.start_pos_y, self.robot.start_pos_z]
-        self.target = AntMazeBulletEnv.targets[self.rs.randint(0, len(AntMazeBulletEnv.targets))]
+        self.target = self.targets[self.rs.randint(0, len(self.targets))]
         super().reset()
 
         # Allows for correct inner reward
